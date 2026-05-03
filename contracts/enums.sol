7ce75f2223662c791ffabc94be432b9af2494666
Ariel Carmen <ariel.ahognisse@epitech.eu>
2026-05-03 15:44:51 +0100

    add: enums demo

---
+++ contracts/enums.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.30;
+
+contract enums_demo {
+
+    enum step {ordered, shipped, delivered}
+
+    struct product {
+        uint _sku;
+        enums_demo.step _step;
+    }
+
+    mapping(address => product) Orders;
+
+    function order(address _clientAddress, uint _sku) public {
+        product memory p = product(_sku, step.ordered);
+        Orders[_clientAddress] = p;
+    }
+
+    function ship(address _clientAddress) public {
+        Orders[_clientAddress]._step = step.shipped;
+    }
+
+    function deliver(address _clientAddress) public {
+        Orders[_clientAddress]._step = step.delivered;
+    }
+
+    function getOrderStep(address _clientAddress) public view returns (step){
+        return Orders[_clientAddress]._step;
+    }
+}
